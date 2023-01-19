ec1c820e99f6dcdbcb178465bb351bc0210cd205
marinesnow34 <m4rinesnow34@gmail.com>
2023-01-19 18:41:04 +0900

    feat: 연결된 api 파싱
    - kickscooters값을 json으로 파싱
    - 파싱 결과를 console에 출력
    - 추후 리팩토링 후 사용

--- src/main/java/com/example/one_eye/utils/KickGoing.java
+++ src/main/java/com/example/one_eye/utils/KickGoing.java
@@ -1,9 +1,11 @@
 package com.example.one_eye.utils;
 
 import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,7 +13,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @SpringBootApplication
 @EnableScheduling
 public class KickGoing {
-    @Scheduled(fixedDelay=2000) // 2초마다 반복
+    @Scheduled(fixedDelay=200000) // 200초마다 반복
     public void getKickGoingScooter() {
         String url = "https://api.kickgoing.io/v4/kickscooters?"
                 + "version=%s&lat=%f&lng=%f&zoom=%f";
@@ -31,6 +33,36 @@ public class KickGoing {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        System.out.println(response.body());
+        JSONObject api = parseJson(response.body());
+        if(api != null) {
+            JSONArray kickScooters = parseJsonArray(api.get("kickscooters").toString());
+            for (Object kickScooter: kickScooters) {
+                JSONObject detail = parseJson(kickScooter.toString());
+                System.out.printf("serial_number: %s, lat: %f, lng: %f\n",
+                        detail.get("serial_number"), detail.get("lat"), detail.get("lng")); // get안에
+            }
+        }
+    }
+
+    private JSONArray parseJsonArray(String str){
+        JSONParser jsonParser = new JSONParser();
+        JSONArray jsonArray = null;
+        try{
+            jsonArray = (JSONArray) jsonParser.parse(str);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return jsonArray;
+    }
+
+    private JSONObject parseJson(String str){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = null;
+        try{
+            jsonObject = (JSONObject) jsonParser.parse(str);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return jsonObject;
     }
 }
