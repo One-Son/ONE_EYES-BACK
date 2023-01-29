783ca39258a5f8742d688c26e80ef493377d2358
Joowon Lim <m4rinesnow34@gmail.com>
2023-01-29 23:51:02 +0900

    fix: bild시 application.properties 받아 오기

--- src/main/java/com/example/one_eye/OneEyeApplication.java
+++ src/main/java/com/example/one_eye/OneEyeApplication.java
@@ -3,6 +3,7 @@ package com.example.one_eye;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@PropertySource(value = "file:/home/opc/one_son_back/application.properties")
 @SpringBootApplication
 public class OneEyeApplication {
 
