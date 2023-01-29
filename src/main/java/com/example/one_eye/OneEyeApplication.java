b3b1b57f19ce8b30327973824db24e7d2decafe5
Joowon Lim <m4rinesnow34@gmail.com>
2023-01-30 00:44:27 +0900

    fix: 패키지 import

--- src/main/java/com/example/one_eye/OneEyeApplication.java
+++ src/main/java/com/example/one_eye/OneEyeApplication.java
@@ -2,6 +2,7 @@ package com.example.one_eye;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value = "file:/home/opc/one_son_back/application.properties")
 @SpringBootApplication
