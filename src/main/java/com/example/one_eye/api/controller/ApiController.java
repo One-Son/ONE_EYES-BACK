7b55636538fbcafe841682d5735370bda9d51657
marinesnow34 <m4rinesnow34@gmail.com>
2023-01-30 11:53:47 +0900

    test: 최종 배포 설정

--- src/main/java/com/example/one_eye/api/controller/ApiController.java
+++ src/main/java/com/example/one_eye/api/controller/ApiController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ApiController {
 
-    private static final long REPEAT_SECOND = 600; //반복 초
+    private static final long REPEAT_SECOND = 60; //반복 초
     @Autowired
     private ApiService apiService;
 
